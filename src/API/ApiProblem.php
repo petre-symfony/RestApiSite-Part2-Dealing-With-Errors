812b58caf73fdce7216ecd970138cfdd765825b8
petrero <petrero@gmail>
2019-01-31 15:04:57 -0600

    Instead of writing error data by hand we model a class get the job done

---
+++ src/API/ApiProblem.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\API;
+
+/**
+ * A wrapper for holding data to be used for a application/problem+json response
+ */
+class ApiProblem {
+	private $statusCode;
+
+	private $type;
+
+	private $title;
+
+	private $extraData = array();
+
+	public function __construct($statusCode, $type, $title){
+		$this->statusCode = $statusCode;
+		$this->type = $type;
+		$this->title = $title;
+	}
+
+	public function toArray(){
+		return array_merge(
+			$this->extraData,
+			array(
+				'status' => $this->statusCode,
+				'type' => $this->type,
+				'title' => $this->title,
+			)
+		);
+	}
+
+	public function set($name, $value){
+		$this->extraData[$name] = $value;
+	}
+
+	public function getStatusCode(){
+		return $this->statusCode;
+	}
+
+	public function getTitle(){
+		return $this->title;
+	}
+}
